04647bf178cf35958eabd9376cd2a2785cd4dceb
hari-sikchi <harshitsikchi8@gmail.com>
2018-03-20 23:05:26 +0530

    Integrated Planner

--- gym_gazebo/envs/husky/gazebo_husky_wall_moving_obstacles.py
+++ gym_gazebo/envs/husky/gazebo_husky_wall_moving_obstacles.py
@@ -3,14 +3,21 @@ import rospy
 import roslaunch
 import time
 import numpy as np
-
+import subprocess
 from gym import utils, spaces
 from gym_gazebo.envs import gazebo_env
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
+
+from std_msgs.msg import Empty as EmptyM
+from nav_msgs.msg import Path
+
 from std_srvs.srv import Empty
-
+import os
 from sensor_msgs.msg import LaserScan
-
+from std_msgs.msg import String	
+from subprocess import call
 from gym.utils import seeding
 
 class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
@@ -31,7 +38,7 @@ class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
 
     def discretize_observation(self,data,new_ranges):
         discretized_ranges = []
-        min_range = 0.2
+        min_range = 0.4
         done = False
         mod = len(data.ranges)/new_ranges
         for i, item in enumerate(data.ranges):
@@ -51,7 +58,7 @@ class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
         return [seed]
 
     def _step(self, action):
-
+    	#  Blocks until a service is available
         rospy.wait_for_service('/gazebo/unpause_physics')
         try:
             self.unpause()
@@ -60,26 +67,37 @@ class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
 
         if action == 0: #FORWARD
             vel_cmd = Twist()
-            vel_cmd.linear.x = 0.1
+            vel_cmd.linear.x = 0.5
             vel_cmd.angular.z = 0.0
             self.vel_pub.publish(vel_cmd)
         elif action == 1: #LEFT
             vel_cmd = Twist()
-            vel_cmd.linear.x = 0.01
+            vel_cmd.linear.x = 0.02
             vel_cmd.angular.z = 0.3
             self.vel_pub.publish(vel_cmd)
         elif action == 2: #RIGHT
             vel_cmd = Twist()
-            vel_cmd.linear.x = 0.01
+            vel_cmd.linear.x = -0.02
             vel_cmd.angular.z = -0.3
             self.vel_pub.publish(vel_cmd)
 
+
+        # Define the states for our MDP
+
+        # Get laserscan data, costmap
         data = None
         while data is None:
             try:
                 data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
             except:
                 pass
+
+
+        # Get current position and velocity
+        odometry=subprocess.check_output(["rosservice","call","gazebo/get_model_state", "{model_name: mobile_base}"])
+       
+
+
 
         rospy.wait_for_service('/gazebo/pause_physics')
         try:
@@ -96,6 +114,7 @@ class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
             else:
                 reward = 1
         else:
+
             reward = -200
 
         return state, reward, done, {}
@@ -104,12 +123,47 @@ class GazeboHuskyWallMovingObstaclesLidarEnv(gazebo_env.GazeboEnv):
         print("Reset simulation")
         # Resets the state of the environment and returns an initial observation.
         rospy.wait_for_service('/gazebo/reset_simulation')
+        empty_msg=EmptyM()
+        pub_reset=rospy.Publisher('/reset_time', EmptyM, queue_size=5)
         try:
             #reset_proxy.call()
+            #pub_reset.publish(empty_msg)
             self.reset_proxy()
+            pub_reset.publish(empty_msg)
+
         except (rospy.ServiceException) as e:
             print ("/gazebo/reset_simulation service call failed")
 
+        pub_goal=rospy.Publisher('/navfn_node/goal', PoseStamped, queue_size=5)
+        goal=PoseStamped()
+        goal.header.frame_id="map"
+        goal.pose.position.x=8.0
+        goal.pose.position.y=8.0
+        goal.pose.position.z=0.0
+        goal.pose.orientation.x=0.0
+        goal.pose.orientation.x=0.0
+        goal.pose.orientation.x=0.26
+        goal.pose.orientation.x=1.0
+        pub_goal.publish(goal)
+        print("Published goal, getting plan!")
+        plan= None
+        ctr =0 
+        while plan is None:
+            if(ctr==5):
+                print("Path not found")
+                break
+
+            try:
+                plan = rospy.wait_for_message('/navfn_node/navfn_planner/plan', Path, timeout=5)
+            except:
+                ctr=ctr+1
+                pass
+
+        print(plan)
+
+
+        #call(["roslaunch", "husky_navigation", "move_base_mapless_demo.launch"])
+        #os.system('roslaunch husky_navigation move_base_mapless_demo.launch')
         # Unpause simulation to make observation
         rospy.wait_for_service('/gazebo/unpause_physics')
         try:
